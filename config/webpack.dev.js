777e919c24a8cf8eb905cc04981d0c4015d39693
Adrien Hobbs <adrien.hobbs@gmail.com>
2019-05-07 11:34:11 -0400

    remove unnecessary code

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -82,8 +82,6 @@ module.exports = {
       template: 'index.ejs'
     }),
     new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
       host: 'localhost',
       port: 3000,
       proxy: 'http://localhost:8080/'
